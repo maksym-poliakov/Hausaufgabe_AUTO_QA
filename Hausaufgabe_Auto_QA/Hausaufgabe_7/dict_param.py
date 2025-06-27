b0a26144c9f81b18ae61effbd8bb0d8d66eb3629
max <max020780@gmail.com>
2025-06-27 19:21:58 +0200

    add files hausaufgabe_7

---
+++ Hausaufgabe_Auto_QA/Hausaufgabe_7/dict_param.py
@@ -0,0 +1,17 @@
+CREATE_EMPLOYEE = {
+"first_name": "MAXIM",
+  "last_name": "MAXIMOV",
+  "middle_name": "MAXIMMAXIMOV",
+  "company_id": 1,
+  "email": "maxim@maximov.com",
+  "phone": "+380681234567",
+  "birthdate": "2015-06-27",
+  "is_active": True
+}
+
+UPDATE_EMPLOYEE = {
+    "last_name": "KIRILOV",
+    "email": "KIRILOV@example.com",
+    "phone": "+386543218678",
+    "is_active": False
+}
